f5ae8ff47fda0901e8be8ca94f72436b260ac9b6
Yadnu <yadneyajoshi3@gmail.com>
2023-07-11 23:37:30 +0530

    Made the code function-based

--- PhoneNumber.py
+++ PhoneNumber.py
@@ -4,7 +4,15 @@ from phonenumbers import geocoder
 from phonenumbers import timezone
 
 number = input("Enter the phonenumber with country code and without any space")
-number = ph.parse(number)
-print(timezone.time_zones_for_number(number))
-print(carrier.name_for_number(number, "en"))
-print(geocoder.description_for_number(number, "en"))
+def getParsedData(number):
+    return ph.parse(number)
+def getTimezone(number):
+    return timezone.time_zones_for_number(number)
+def getCarrierName(number):
+    return carrier.name_for_number(number, "en")
+def getDescription(number):
+    return geocoder.description_for_number(number, "en")
+number=getParsedData(number)
+PhoneTimezone=getTimezone(number)
+PhoneCarrier=getCarrierName(number)
+PhoneDescription=getDescription(number)
